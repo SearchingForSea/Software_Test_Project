cf07a8410434cde94f488667ac92e7682180aac2
LUOJUN <916336779@qq.com>
2023-05-02 17:35:48 +0800

    Solve the first problem.

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <nav-home />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavHome from "@/components/NavHome";
+export default {
+  name: "HomePage",
+  components: {NavHome},
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  display: flex;
+}
+</style>
